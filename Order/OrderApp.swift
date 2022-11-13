cb1dbadbf9a4df4bbd57d4bb24ec0146e2d642b1
Joon1313 <wkdrud13@gmail.com>
2022-11-13 17:13:43 +0900

    Initial Commit

---
+++ Order/OrderApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrderApp.swift
+//  Order
+//
+//  Created by 장경준 on 2022/11/13.
+//
+
+import SwiftUI
+
+@main
+struct OrderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
